d91ae8a285ef9d3a896d51de12101446e15481d5
haruki <haruki@armagetron>
2013-11-13 16:33:50 -0200

    aeho

--- src/com/aehooo/tdsqn/MainActivity.java
+++ src/com/aehooo/tdsqn/MainActivity.java
@@ -1,14 +1,15 @@
 package com.aehooo.tdsqn;
 
 import org.andengine.engine.camera.Camera;
-import org.andengine.engine.camera.SmoothCamera;
-import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.engine.options.EngineOptions;
 import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+import android.util.Log;
 
 import com.aehooo.tdsqn.resources.ImageAlligator3000;
 import com.aehooo.tdsqn.resources.TextureName;
@@ -31,16 +32,55 @@ public class MainActivity extends SimpleBaseGameActivity {
 	@Override
 	protected void onCreateResources() {
 		// TODO Auto-generated method stub
-		
+		ImageAlligator3000.initialize(this);
 	}
 
 	@Override
 	protected Scene onCreateScene() {
-		ImageAlligator3000.initialize(this);
 		Sprite bgmapa1 = new Sprite(0, 0,
 				ImageAlligator3000.getTexture(TextureName.MAPA1_BG),
-				ImageAlligator3000.getVertexBufferObjectManager());
+				ImageAlligator3000.getVertexBufferObjectManager()) {
+			
+			float antx = 0;
+			float anty = 0;
+			
+			float posx = 0;
+			float posy = 0;
+			
+			@Override
+			public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
+					float pTouchAreaLocalX, float pTouchAreaLocalY) {
+				if (pSceneTouchEvent.isActionDown()) {
+					Log.i("AEHO.MainActivity", "ActionDown");
+					antx = pSceneTouchEvent.getX();
+					anty = pSceneTouchEvent.getY();
+					
+					posx = this.getX();
+					posy = this.getY();
+				}
+				else if(pSceneTouchEvent.isActionUp()) {
+					Log.i("AEHO.MainActivity", "ActionUp");
+				}
+				else if(pSceneTouchEvent.isActionMove()) {
+					Log.i("AEHO.MainActivity", "ActionMove");
+					float newx = posx + (pSceneTouchEvent.getX() - antx);
+					float newy = posy + (pSceneTouchEvent.getY() - anty);
+					
+					if (newx > 0)
+						newx = 0;
+					if (newy > 0)
+						newy = 0;
+					
+					this.setPosition(newx, newy);
+				}
+				
+				return true;
+			}
+		};
+		
 		LevelScene scene = new LevelScene(bgmapa1);
+		
+		
 		
 	    return scene;
 	}
--- src/com/aehooo/tdsqn/scenes/LevelScene.java
+++ src/com/aehooo/tdsqn/scenes/LevelScene.java
@@ -1,5 +1,6 @@
 package com.aehooo.tdsqn.scenes;
 
+import org.andengine.entity.modifier.MoveModifier;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.SpriteBackground;
 import org.andengine.entity.sprite.Sprite;
@@ -8,6 +9,9 @@ public class LevelScene extends Scene {
 	
 	public LevelScene(Sprite bg) {
 		super();
-		this.setBackground(new SpriteBackground(bg));
+		this.attachChild(bg);
+		registerTouchArea(bg);
+		this.setTouchAreaBindingOnActionDownEnabled(true);
+		this.setTouchAreaBindingOnActionMoveEnabled(true);
 	}
 }
